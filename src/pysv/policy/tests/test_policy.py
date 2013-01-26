4378672453fcbe98d601661a7bdd4e16fc1f2b2c
Christine Koppelt <ch.ko123@googlemail.com>
2013-01-26 15:51:53 +0100

    add test

--- src/pysv/policy/tests/test_policy.py
+++ src/pysv/policy/tests/test_policy.py
@@ -6,7 +6,7 @@ from pysv.policy.testing import\
     PYSV_POLICY_INTEGRATION_TESTING
 
 
-class TestExample(unittest.TestCase):
+class TestPolicy(unittest.TestCase):
 
     layer = PYSV_POLICY_INTEGRATION_TESTING
     
@@ -14,8 +14,9 @@ class TestExample(unittest.TestCase):
         self.app = self.layer['app']
         self.portal = self.layer['portal']
         self.qi_tool = getToolByName(self.portal, 'portal_quickinstaller')
+        self.ttool = getToolByName(self.portal, "portal_types")
     
-    def test_product_is_installed(self):
+    def test_policy_is_installed(self):
         """ Validate that our products GS profile has been run and the product 
             installed
         """
@@ -23,3 +24,9 @@ class TestExample(unittest.TestCase):
         installed = [p['id'] for p in self.qi_tool.listInstalledProducts()]
         self.assertTrue(pid in installed,
                         'package appears not to have been installed')
+
+
+    def test_FormFolder_exists (self):
+        self.assertTrue ('FormFolder' in self.ttool.listContentTypes())
+
+
