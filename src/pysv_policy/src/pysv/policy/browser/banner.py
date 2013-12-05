a5d0afa57710084b15fad7465c3b34f11b51186a
Jens W. Klein <jens@bluedynamics.com>
2013-12-05 20:39:08 +0100

    minor code cleanup

--- src/pysv_policy/src/pysv/policy/browser/banner.py
+++ src/pysv_policy/src/pysv/policy/browser/banner.py
@@ -1,20 +1,17 @@
 from Acquisition import aq_inner
 from Acquisition import aq_parent
-
 from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.interfaces.siteroot import IPloneSiteRoot
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-
-from plone.memoize.instance import memoize
 from plone.app.layout.viewlets.common import ViewletBase
-
+from plone.memoize.instance import memoize
+from pysv.policy.browser.interfaces import IBanner
 from zope.component import getMultiAdapter
 from zope.viewlet.interfaces import IViewlet
 
-from pysv.policy.browser.interfaces import IBanner
-
 import random
+
 
 class BannerViewlet(ViewletBase):
     """ Smart Teasers with scrollable. text and link
@@ -26,18 +23,18 @@ class BannerViewlet(ViewletBase):
         return getToolByName(self.context, 'portal_catalog')
 
     def random_banner(self):
-        if self.context.portal_type in ['Folder',] or IPloneSiteRoot.providedBy(self.context):
-            bp = self.context
-        else:
+        bp = self.context
+        if self.context.portal_type not in ['Folder', ] \
+           and not IPloneSiteRoot.providedBy(self.context):
             bp = aq_parent(self.context)
 
         path = '/'.join(bp.getPhysicalPath())
-        banner = self.portal_catalog(portal_type = 'Teaser',
-                                           path = dict(query=path),
-                                           review_state  = 'published',
-                                           )
+        banner = self.portal_catalog(
+            portal_type='Teaser',
+            path=dict(query=path),
+            review_state='published',
+        )
         self.scroll = len(banner) > 1
-
         banner = list(banner)
         random.shuffle(banner)
         return banner
--- src/pysv_policy/src/pysv/policy/browser/frontpage.py
+++ src/pysv_policy/src/pysv/policy/browser/frontpage.py
@@ -1,40 +1,45 @@
 from Acquisition import aq_inner
-
+from Products.CMFCore.utils import getToolByName
+from DateTime.DateTime import DateTime
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-
-from Products.CMFCore.utils import getToolByName
-
 from plone.memoize.instance import memoize
 from plonede.content.interfaces import ITeaser
 
-from DateTime.DateTime import DateTime
 
 class FrontpageView(BrowserView):
-    __call__ = ViewPageTemplateFile('frontpage.pt')
 
     def _teasers(self):
         context = aq_inner(self.context)
         catalog = getToolByName(context, 'portal_catalog')
         path = '/'.join(context.getPhysicalPath())
-        results = catalog(portal_type='Teaser', review_state='published', sort_on='id', path=path)
+        results = catalog(
+            portal_type='Teaser',
+            review_state='published',
+            sort_on='id',
+            path=path
+        )
         return results
 
     def _events(self):
         context = aq_inner(self.context)
         catalog = getToolByName(context, 'portal_catalog')
-        results = catalog(portal_type='Event', review_state='published', end={'query': DateTime(), 'range': 'min'}, sort_on='start')[:5]
+        results = catalog(
+            portal_type='Event',
+            review_state='published',
+            end={'query': DateTime(), 'range': 'min'},
+            sort_on='start'
+        )[:5]
         return results
 
     def teasers(self):
         result = self._teasers()
-        
         teaser = []
         for t in result:
             obj = t.getObject()
             teaser.append(obj)
-            
+
         return teaser
-    
+
     def events(self):
-        return self._events() 
+        return self._events()
