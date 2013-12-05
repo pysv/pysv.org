6fee44b8a4be9f92afe8126bec7a6fbcd8843355
Jens W. Klein <jens@bluedynamics.com>
2013-12-05 20:19:45 +0100

    added solgema fullcalendar. this is is a step in the right direction to get #26 done

--- src/pysv_policy/setup.py
+++ src/pysv_policy/setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup, find_packages
-import os
 
 version = '2.0'
 
@@ -14,36 +13,40 @@ long_description = (
     open('CHANGES.txt').read()
     + '\n')
 
-setup(name='pysv.policy',
-      version=version,
-      description="Policy for the pysv website",
-      long_description=long_description,
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-        "Programming Language :: Python",
-        ],
-      keywords='',
-      author='Python Software Verband',
-      author_email='info@python-verband.org',
-      url='https://github.com/pysv/pysv_policy',
-      license='gpl',
-      packages=find_packages('src'),
-      package_dir = {'': 'src'},
-      namespace_packages=['pysv'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'collective.quickupload',
-          'Products.PloneFormGen',
-          'plonede.content',
-          'z3c.jbot',
+setup(
+    name='pysv.policy',
+    version=version,
+    description="Policy for the pysv website",
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+      "Programming Language :: Python",
       ],
-      extras_require={'test': ['plone.app.testing']},
-      entry_points="""
-      # -*- Entry points: -*-
-  	  [z3c.autoinclude.plugin]
-  	  target = plone
-      """,
-      )
+    keywords='',
+    author='Python Software Verband',
+    author_email='info@python-verband.org',
+    url='https://github.com/pysv/pysv_policy',
+    license='gpl',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['pysv'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Pillow',
+        'Plone',
+        'z3c.jbot',
+        'plonede.content',
+        'Solgema.fullcalendar',
+        'collective.quickupload',
+        'Products.PloneFormGen',
+    ],
+    extras_require={'test': ['plone.app.testing']},
+    entry_points="""
+    # -*- Entry points: -*-
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
