6d0a8527e7181810139bba8c5f1f4f7c1d817dfd
Jens W. Klein <jens@bluedynamics.com>
2013-12-05 20:22:57 +0100

    pep8 et al

--- src/pysv_policy/src/pysv/policy/adapter.py
+++ src/pysv_policy/src/pysv/policy/adapter.py
@@ -1,10 +1,14 @@
 from plone.app.users.browser.personalpreferences import UserDataPanelAdapter
 
+
 class EnhancedUserDataPanelAdapter(UserDataPanelAdapter):
     """
     """
+
     def get_isVisibleFor(self):
         return self.context.getProperty('isVisibleFor', '')
+
     def set_isVisibleFor(self, value):
         return self.context.setMemberProperties({'isVisibleFor': value})
+
     isVisibleFor = property(get_isVisibleFor, set_isVisibleFor)
